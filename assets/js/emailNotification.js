2736355fc7d301aa09af13af6cba2f74e34bcff5
Zach Resnick <zachary.l.resnick@gmail.com>
2019-10-06 16:01:22 -0400

    Feat: Contact form works completely

--- assets/js/emailNotification.js
+++ assets/js/emailNotification.js
@@ -9,20 +9,39 @@ button.addEventListener('click', evt => {
   let sender = document.getElementById('sender').value;
   let content = document.getElementById('emailContent').value;
 
-  let templateParams = {
-    email,
-    sender,
-    content,
-  };
+  if (email && sender && content) {
+    let templateParams = {
+      email,
+      sender,
+      content,
+    };
 
-  let serviceId = 'default_service';
-  let templateId = 'template_qbGQANvs';
-  emailjs.send(serviceId, templateId, templateParams).then(
-    function(response) {
-      console.log('SUCCESS!', response.status, response.text);
-    },
-    function(error) {
-      console.log('FAILED...', error);
+    let serviceId = 'default_service';
+    let templateId = 'template_qbGQANvs';
+    emailjs.send(serviceId, templateId, templateParams).then(
+      function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+      },
+      function(error) {
+        console.log('FAILED...', error);
+      }
+    );
+
+    document.getElementById('emailAddress').value = '';
+    document.getElementById('sender').value = '';
+    document.getElementById('emailContent').value = '';
+
+    location.hash = 'thanks';
+    setTimeout(function() {
+      location.hash = '';
+    }, 5000);
+    if (document.getElementById('addedNode')) {
+      document.getElementById('addedNode').remove();
     }
-  );
+  } else if (!document.getElementById('addedNode')) {
+    let node = document.createElement('LI');
+    node.innerText = 'Please fill out all fields before submitting!';
+    node.id = 'addedNode';
+    document.getElementById('contactActions').appendChild(node);
+  }
 });
