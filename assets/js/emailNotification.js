8551be4bb73fa517843b887e3ecb94ba149f66ac
Zach Resnick <zachary.l.resnick@gmail.com>
2019-10-05 23:18:15 -0400

    Feat: Emails are sending!!

--- assets/js/emailNotification.js
+++ assets/js/emailNotification.js
@@ -4,5 +4,25 @@ const button = document.getElementById('sendMessageButton');
 
 button.addEventListener('click', evt => {
   evt.preventDefault();
-  console.log('Hello World');
+
+  let email = document.getElementById('emailAddress').value;
+  let sender = document.getElementById('sender').value;
+  let content = document.getElementById('emailContent').value;
+
+  let templateParams = {
+    email,
+    sender,
+    content,
+  };
+
+  let serviceId = 'default_service';
+  let templateId = 'template_qbGQANvs';
+  emailjs.send(serviceId, templateId, templateParams).then(
+    function(response) {
+      console.log('SUCCESS!', response.status, response.text);
+    },
+    function(error) {
+      console.log('FAILED...', error);
+    }
+  );
 });
